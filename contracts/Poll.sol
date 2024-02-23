a5cb11efd93b11f4dd22e626346e3700c4d7055f
PhantomOz <faniogor@gmail.com>
2024-02-23 19:58:22 +0100

    this commit will initialize Poll constructor

--- contracts/Poll.sol
+++ contracts/Poll.sol
@@ -2,5 +2,19 @@
 pragma solidity ^0.8.19;
 
 contract Poll {
-    
+    string public question; 
+    string[] public options;
+    mapping(address => uint) public votes;
+    mapping(address => bool) public voted;
+    uint256 public endTime;
+
+    constructor(string memory _question, string[] memory _options, uint256 _duration) {
+        require(bytes(_question).length > 0, "Question cannot be empty");
+        require(_options.length > 1, "Options must be more than one");
+        require(_duration > 0, "Duration must be positive");
+        uint256 _endTime = block.timestamp + _duration;
+        question = _question;
+        options = _options;
+        endTime = _endTime;
+    }
 }
--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -1,4 +1,75 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-contract Voting {}
+import {Poll} from "./Poll.sol";
+
+contract Voting {
+    
+
+    // An array to store all the polls
+    Poll[] public polls;
+
+    // An event to emit when a new poll is created
+    event PollCreated(uint pollId, string question, string[] options, uint endTime);
+
+    // An event to emit when a vote is cast
+    event PollVoted(uint pollId, address voter, uint option);
+
+    // A modifier to check if the poll exists
+    modifier pollExists(uint pollId) {
+        require(pollId < polls.length, "Poll does not exist");
+        _;
+    }
+
+    // A modifier to check if the poll is open
+    modifier pollOpen(uint pollId) {
+        require(block.timestamp < polls[pollId].endTime, "Poll is closed");
+        _;
+    }
+
+    // A modifier to check if the voter has not voted
+    modifier notVoted(uint pollId, address voter) {
+        require(!polls[pollId].voted[voter], "Already voted");
+        _;
+    }
+
+    // A modifier to check if the option is valid
+    modifier validOption(uint pollId, uint option) {
+        require(option < polls[pollId].options.length, "Invalid option");
+        _;
+    }
+
+    // A function to create a new poll
+    function createPoll(string memory _question, string[] memory _options, uint256 _duration) public {
+        Poll newPoll = new Poll(_question, _options, _duration);
+        polls.push(newPoll);
+        uint pollId = polls.length - 1;
+        emit PollCreated(pollId, _question, _options, (block.timestamp + _duration));
+    }
+
+    // A function to vote on a poll
+    function votePoll(uint pollId, uint option) public pollExists(pollId) pollOpen(pollId) notVoted(pollId, msg.sender) validOption(pollId, option) {
+        polls[pollId].votes[option]++;
+        polls[pollId].voted[msg.sender] = true;
+        emit PollVoted(pollId, msg.sender, option);
+    }
+
+    // A function to get the total votes for an option
+    function getVotes(uint pollId, uint option) public view pollExists(pollId) validOption(pollId, option) returns (uint) {
+        return polls[pollId].votes[option];
+    }
+
+    // A function to get the winner of a poll
+    function getWinner(uint pollId) public view pollExists(pollId) returns (uint) {
+        require(block.timestamp >= polls[pollId].endTime, "Poll is not over yet");
+        uint maxVotes = 0;
+        uint winner = 0;
+        for (uint i = 0; i < polls[pollId].options.length; i++) {
+            if (polls[pollId].votes[i] > maxVotes) {
+                maxVotes = polls[pollId].votes[i];
+                winner = i;
+            }
+        }
+        return winner;
+    }
+}
